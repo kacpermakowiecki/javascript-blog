1a6e83e50d1bb52434e2cae31eb73c3ca60cfca4
Kacper Makowiecki <makkacperr@gmail.com>
2021-12-08 14:25:28 +0100

    add class 'active' to the correct article

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
 'use strict';
 
-function titleClickHandler(event){
+function titleClickHandler(event) {
   event.preventDefault();
   const clickedElement = this;
   console.log('Link was clicked!');
 
   /* remove class 'active' from all article links  */
-
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for (let activeLink of activeLinks) {
@@ -14,12 +13,10 @@ function titleClickHandler(event){
   }
 
   /* add class 'active' to the clicked link */
-
   clickedElement.classList.add('active');
-  console.log('clickedElement:', clickedElement);
+  console.log(`clickedElement: ${clickedElement}`);
 
   /* remove class 'active' from all articles */
-  
   const activeArticles = document.querySelectorAll('.post');
 
   for (let activeArticle of activeArticles) {
@@ -29,10 +26,13 @@ function titleClickHandler(event){
   /* get 'href' attribute from the clicked link */
   const articleSelector = clickedElement.getAttribute('href');
   console.log(articleSelector);
-  
+
   /* find the correct article using the selector (value of 'href' attribute) */
+  const targetArticle = document.querySelector(articleSelector);
+  console.log(targetArticle);
 
   /* add class 'active' to the correct article */
+  targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
