7b10ee46def56d083f5a1db6e342510fbea7cc2b
Kacper Makowiecki <makkacperr@gmail.com>
2021-12-08 13:48:17 +0100

    /* add class 'active' to the clicked link */

--- js/script.js
+++ js/script.js
@@ -1,13 +1,30 @@
 'use strict';
 
 function titleClickHandler(event){
-  console.log(event);
+  const clickedElement = this;
+  console.log('Link was clicked!');
 
   /* remove class 'active' from all article links  */
 
+  const activeLinks = document.querySelectorAll('.titles a.active');
+
+  for (let activeLink of activeLinks) {
+    activeLink.classList.remove('active');
+  }
+
   /* add class 'active' to the clicked link */
 
+  clickedElement.classList.add('active');
+  console.log('clickedElement:', clickedElement);
+
   /* remove class 'active' from all articles */
+  
+  const activeArticles = document.querySelectorAll('.post');
+
+  for (let activeArticle of activeArticles) {
+    activeArticle.classList.remove('active');
+  }
+
 
   /* get 'href' attribute from the clicked link */
 
@@ -18,6 +35,6 @@ function titleClickHandler(event){
 
 const links = document.querySelectorAll('.titles a');
 
-for(let link of links){
+for (let link of links) {
   link.addEventListener('click', titleClickHandler);
 }
