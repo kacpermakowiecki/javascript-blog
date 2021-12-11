bea33d98aa7bc4aa12565e2a96a0602e71e3c832
Kacper Makowiecki <makkacperr@gmail.com>
2021-12-11 11:24:33 +0100

    modified and debug js

--- js/script.js
+++ js/script.js
@@ -35,7 +35,45 @@ function titleClickHandler(event) {
   targetArticle.classList.add('active');
 }
 
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks() {
+
+  /* remove contents of titleList */
+  const titleList = document.querySelector(optTitleListSelector);
+  titleList.innerHTML = '';
+  console.log(titleList);
+
+  /* for each article */
+  const articles = document.querySelectorAll(optArticleSelector);
+  console.log(articles);
+
+  let html = '';
+  for (let article of articles) {
+    console.log(article);
+
+    /* get the article id */
+    const articleId = article.getAttribute('id');
+
+    /* find the title element, get the title from the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+    /* create HTML of the link */
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    console.log(`linkHTML ${linkHTML}`);
+
+    /* insert link into titleList */
+    html = html + linkHTML;
+  }
+  titleList.innerHTML = html;
+
+}
+
+generateTitleLinks();
 const links = document.querySelectorAll('.titles a');
+console.log(links);
 
 for (let link of links) {
   link.addEventListener('click', titleClickHandler);
